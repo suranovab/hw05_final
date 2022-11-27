f6dd9b1aad89c8962500e700d76f862c086c7f29
Алексей Суранов <suranovab@52>
2022-11-27 19:03:08 +0300

    Правка перед ревью 1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -92,7 +92,7 @@ class Comment(models.Model):
 class Follow(models.Model):
     user = models.ForeignKey(
         User,
-        on_delete=None,
+        on_delete=models.CASCADE,
         related_name="follower"
     )
     author = models.ForeignKey(
